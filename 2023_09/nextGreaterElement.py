c44538e6c4b4be02a204285ae7a1d1345573f6aa
Juan Gerardo Franco <juangerardofranco@gmail.com>
2023-09-10 20:00:40 -0500

    a little of format to the code

--- 2023_09/nextGreaterElement.py
+++ 2023_09/nextGreaterElement.py
@@ -13,7 +13,7 @@ Return an array ans of length nums1.length such that ans[i] is the next greater 
 """
 
 class Solution:
-  def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+  def nextGreaterElement(self, nums1, nums2):
         
     stack = []
     nextGreater = {}
@@ -22,5 +22,5 @@ class Solution:
       if stack and num > stack[-1]:
         nextGreater[stack.pop()] = num
       stack.append(num)
-      
+
     return [nextGreater.get(num, -1) for num in nums1]
