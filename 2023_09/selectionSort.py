d6b9ca142f1448ea25caf6ec47987e8531f1839f
Juan Gerardo Franco <juangerardofranco@gmail.com>
2023-09-16 10:11:24 -0500

    practice of selection sort

--- 2023_09/selectionSort.py
+++ 2023_09/selectionSort.py
@@ -1,3 +1,36 @@
+
+'''
+🔎 EXPLORE
+What are some other insightful & revealing test cases?
+ 
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: selection sort
+Time: O(n squared)
+Space: O(1)
+ 
+
+📆 PLAN
+Outline of algorithm #: 
+traverse the array, i index:
+  initialize current minimum idx with i
+  traverse the array from the next position to the end of the array, j index: 
+    if the number at j is smaller than the current minumn. update minimum
+
+  if the minimum idx is different than i idx:
+    swap elements
+return array
+
+🛠️ IMPLEMENT
+Write your algorithm.
+ 
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+'''
+
 def sortArray(nums):
   
   for idx in range(len(nums)):
