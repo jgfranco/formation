ddcb9791707e17c4fef9be849dce2a55234c2dd7
Juan Gerardo Franco <juangerardofranco@gmail.com>
2023-10-08 12:36:00 -0500

    Given an array of positive integers, find the first element that occurs k number of times

---
+++ 2023_10/findFirstElementRepeatedKTimes.py
@@ -0,0 +1,32 @@
+'''
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+✏️ Description
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Q. Given an array of positive integers, find the first element 
+that occurs k number of times. If no element occurs k times, 
+return -1, and you may assume k is greater than 0. 
+
+Examples:
+• Given an array: [1, 2, 2, 3, 3], k: 2 // returns 2
+• Given an array: [], k: 1 // returns -1
+
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+🟦 Python
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+'''
+def firstKTimes(input: [int], k: int) -> int:
+  numMap = {}
+
+  for num in input:
+    numMap[num] = numMap.get(num,0) +1
+  filtered = list( filter(lambda x: x[1] == k, numMap.items()))
+
+  if len(filtered) == 0: return -1
+
+  return filtered[0][0]
+
+# Test Cases
+print(firstKTimes([1, 2, 2, 3, 3], 2)) # 2
+print(firstKTimes([1, 2, 2, 3, 3], 3)) # -1
+print(firstKTimes([], 1)) # -1
+print(firstKTimes([1, 2, 2, 3, 3, 2], 2)) # 3
