4e947ec1433f077145948c1375c45be2b8112725
Juan Gerardo Franco <juangerardofranco@Juans-MacBook-Pro.attlocal.net>
2025-03-20 09:46:36 -0500

    greatest common divisor of Strings - Leetcode

---
+++ 2025_03/greatestCommonDivisorOfStrings.py
@@ -0,0 +1,50 @@
+"""
+https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75"
+
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+
+"""
+
+class Solution:
+    """public int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }    
+    }"
+    """
+    def gcd(self, x: int, y: int) -> int:
+        if y == 0:
+            return x
+        else:
+            return self.gcd(y, x % y)
+        
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return None
+        
+        max_length = self.gcd(len(str1), len(str2))
+        return str1[:max_length]
+
+
+print(Solution().gcdOfStrings("ABCABC", "ABC")) # "ABC"
+print(Solution().gcdOfStrings("ABABAB", "ABAB")) # "AB"
+print(Solution().gcdOfStrings("LEET", "CODE")) # ""
