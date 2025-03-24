cca714b8f8b7b65d5ac3313f5f466bcd55ec4092
Juan Gerardo Franco <juangerardofranco@gmail.com>
2025-03-23 22:17:09 -0500

    reversal of vowels in a string - leetcode

--- 2025_03/reverseVowelsOfAString.py
+++ 2025_03/reverseVowelsOfAString.py
@@ -1,4 +1,10 @@
 '''
+https://leetcode.com/problems/reverse-vowels-of-a-string/description/?envType=study-plan-v2&envId=leetcode-75
+
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
+
 '''
 
 
