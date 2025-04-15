fb8383467b369f39b8c8c675955e97d56feeedc1
Juan Gerardo Franco <juangerardofranco@gmail.com>
2025-04-15 18:16:28 -0500

    prefix sum - leetcode

---
+++ 2025_04/findPivotIndex.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/find-pivot-index/description/?envType=study-plan-v2&envId=leetcode-75
+"""
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+        p = 0
+
+        leftSum = 0
+        rightSum = sum(nums) - nums[0]
+
+        while p< len(nums):
+
+            if leftSum == rightSum:
+                return p
+            
+            leftSum += nums[p]
+            p += 1
+            if p < len(nums):
+                rightSum  -= nums[p]
+            
+        return -1
+
