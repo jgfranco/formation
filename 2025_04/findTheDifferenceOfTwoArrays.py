005921a954803252793e29f8300cc34e096490d3
Juan Gerardo Franco <juangerardofranco@gmail.com>
2025-04-16 14:25:27 -0500

    hashmap and set problem - leetcode

---
+++ 2025_04/findTheDifferenceOfTwoArrays.py
@@ -0,0 +1,22 @@
+"""
+
+"""
+
+class Solution:
+    def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
+        
+        nums1Set = set(nums1)
+        nums2Set = set(nums2)
+
+        notInNums2 = set()
+        notInNums1 = set()
+
+        for num in nums1Set:
+            if num not in nums2Set:
+                notInNums2.add(num)
+        
+        for num in nums2Set:
+            if num not in nums1Set:
+                notInNums1.add(num)
+
+        return [list(notInNums2), list(notInNums1)]
