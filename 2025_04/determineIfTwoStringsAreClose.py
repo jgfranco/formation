2a62d0ee53faf489d47457062a082ae0b3e445a9
Juan Gerardo Franco <juangerardofranco@gmail.com>
2025-04-22 14:44:13 -0500

    Use of hashmaps and sets

--- 2025_04/determineIfTwoStringsAreClose.py
+++ 2025_04/determineIfTwoStringsAreClose.py
@@ -8,9 +8,19 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2): return False
 
-        freq1 = Counter(word1)
-        freq2 = Counter(word2)
+        freq1 = {}
+        for char in word1:
+            freq1[char] = freq1.get(char, 0) + 1
+
+        freq2 = {}
+        for char in word1:
+            freq2[char] = freq2.get(char, 0) + 1
 
         if set(freq1.keys()) != set(freq2.keys()): return False
 
         return sorted(freq1.values()) == sorted(freq2.values())
+    
+
+s = Solution()
+
+print(s.closeStrings("abc", "acb"))
