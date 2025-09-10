421325ac1b6756bfcc43df02163ec2995844acd8
Gerardo Franco <juangerardofranco@gmail.com>
2025-09-10 09:44:16 -0500

    recursive examination of an array

---
+++ 2025_09/numberOfTimesSixAppears.py
@@ -0,0 +1,30 @@
+"""
+Given an array of ints, compute recursively the number of times that the value 6 
+appears in the array. We'll use the convention of considering only the part of 
+the array that begins at the given index. In this way, a recursive call can pass 
+index+1 to move down the array. The initial call will pass in index as 0.
+
+Example(s)
+array6([1, 2, 6], 0) == 1
+array6([6, 6], 0) == 2
+array6([1, 2, 3, 4], 0) == 0
+"""
+
+
+def array6(nums: list[int], index: int) -> int:
+
+    if len(nums) <= 0 or index >= len(nums):
+        return 0
+
+    if nums[index] == 6:
+        return 1 + array6(nums, index+1)
+    
+    return array6(nums, index+1)
+
+    
+
+print(array6([1,2,3,4,5,6], 0), "expect 1")
+print(array6([], 0), "expect 0")
+print(array6([1], 0), "expect 0")
+
+print(array6([6,6,6], 0), "expect 3")
